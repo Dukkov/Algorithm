4b80f55be3d684be7c2b247afbb627f01847ace1
Dukov <rhtkdals135@gmail.com>
2023-11-22 18:38:46 +0900

    hashing done

--- javascript/hashing/3_42577.js
+++ javascript/hashing/3_42577.js
@@ -1,11 +1,21 @@
+//전화번호 목록
+//https://school.programmers.co.kr/learn/courses/30/lessons/42577
 function solution(phone_book) {
     const phoneNumObj = {};
 
     phone_book.forEach(element => {
-        phoneNumObj[element] = element.length;
+        phoneNumObj[element] = 0;
     });
-    console.log(phoneNumObj);
+
+    for (element of phone_book) {
+        for (let i = 1; i < element.length; i++) {
+            if (element.slice(0, i) in phoneNumObj)
+                return (false);
+        }
+    }
+    return (true);
 }
 
-const phone_book = ["12", "123", "1235", "567", "88"];
-solution(phone_book);
+// // const phone_book = ["12", "123", "1235", "567", "88"];
+// const phone_book = ["123", "456", "789"];
+// console.log(solution(phone_book));
