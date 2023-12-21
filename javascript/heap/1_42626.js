ebce66d7d97c23999f13c12b4b419500fe902527
rhtkdals135 <rhtkdals135@gmail.com>
2023-12-21 23:59:15 +0900

    heap 1st problem failed

--- javascript/heap/1_42626.js
+++ javascript/heap/1_42626.js
@@ -1,6 +1,43 @@
 // 더 맵게
 // https://school.programmers.co.kr/learn/courses/30/lessons/42626
 function solution(scoville, K) {
-    var answer = 0;
-    return answer;
+    const heapInsert = (arr, element) => {
+        arr.push(element);
+        let elementIdx = arr.length - 1;
+        let parentIdx = Math.floor(elementIdx / 2);
+        
+        while (parentIdx > 0 && arr[parentIdx] > arr[elementIdx]) {
+            [arr[parentIdx], arr[elementIdx]] = [arr[elementIdx], arr[parentIdx]];
+            elementIdx = parentIdx;
+            parentIdx = Math.floor(elementIdx / 2);
+        }
+
+        return (arr);
+    };
+
+    const heapDelete = (arr) => {
+        arr[1] = arr.pop();
+
+        let elementIdx = 1;
+        let firstChildIdx = elementIdx * 2;
+        let secondChildIdx = (elementIdx * 2) + 1;
+        let smallestIdx;
+
+        while(secondChildIdx <= arr.length - 1) {
+            smallestIdx = arr[firstChildIdx] < arr[secondChildIdx] ? firstChildIdx : secondChildIdx;
+            if (arr[elementIdx] > arr[smallestIdx]) {
+                [arr[elementIdx], arr[smallestIdx]] = [arr[smallestIdx], arr[elementIdx]];
+                elementIdx = smallestIdx;
+                firstChildIdx = elementIdx * 2;
+                secondChildIdx = (elementIdx * 2) + 1;
+            }
+            else
+                break;
+        }
+
+        if () {
+
+        }
+        return (arr);
+    };
 }
