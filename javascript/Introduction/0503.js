6e66ea72fddc64dff67bc040f60cec0d2079666d
dukkov <dukov.ko.dev@gmail.com>
2024-05-03 23:14:19 +0900

    Day5 done

---
+++ javascript/Introduction/0503.js
@@ -0,0 +1,26 @@
+// function solution(price) {
+//   if (price >= 500000) return parseInt(price * 0.8);
+//   else if (price >= 300000) return parseInt(price * 0.9);
+//   else if (price >= 100000) return parseInt(price * 0.95);
+
+//   return price;
+// }
+
+// ---
+
+// function solution(money) {
+//   const cups = parseInt(money / 5500);
+//   return [cups, money - cups * 5500];
+// }
+
+// ---
+
+// function solution(age) {
+//   return 2023 - age;
+// }
+
+// ---
+
+function solution(num_list) {
+  return num_list.reverse();
+}
