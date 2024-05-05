ac3bcef423e402ff86b2d58e1e6f44623fe9597d
Dukov <rhtkdals135@gmail.com>
2024-05-05 23:55:44 +0900

    Day7 start

---
+++ javascript/Introduction/0505.js
@@ -0,0 +1,9 @@
+// function solution(my_string, letter) {
+//   const stringArr = [...my_string];
+
+//   return stringArr.filter((char) => char !== letter).join('');
+// }
+
+// ---
+
+function solution(angle) {}
