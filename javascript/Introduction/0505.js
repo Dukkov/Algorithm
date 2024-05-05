af8e2bb976e019826d633a605f79569be0db6f4b
Dukov <rhtkdals135@gmail.com>
2024-05-06 01:14:39 +0900

    Day7 done

--- javascript/Introduction/0505.js
+++ javascript/Introduction/0505.js
@@ -6,4 +6,24 @@
 
 // ---
 
-function solution(angle) {}
+// function solution(angle) {
+//   if (angle < 90) return 1;
+//   if (angle === 90) return 2;
+//   if (angle < 180) return 3;
+
+//   return 4;
+// }
+
+// ---
+
+// function solution(n, k) {
+//   return n * 12000 + (k - parseInt(n / 10)) * 2000;
+// }
+
+// ---
+
+function solution(n) {
+  const arr = [...Array(parseInt(n / 2) + 1)].map((v, i) => i * 2);
+
+  return arr.reduce((acc, val) => acc + val, 0);
+}
