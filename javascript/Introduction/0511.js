6cdf1befe2c340d4c1c6da25efb6caef488cd711
dukkov <dukov.ko.dev@gmail.com>
2024-05-14 22:07:11 +0900

    Day12 done

--- javascript/Introduction/0511.js
+++ javascript/Introduction/0511.js
@@ -1,4 +1,45 @@
-function solution(my_string) {
-  var answer = '';
-  return answer;
+// function solution(my_string) {
+//   return [...my_string]
+//     .filter(
+//       (char) =>
+//         char !== 'a' &&
+//         char !== 'e' &&
+//         char !== 'i' &&
+//         char !== 'o' &&
+//         char !== 'u'
+//     )
+//     .join('');
+// }
+
+// ---
+
+// function solution(my_string) {
+//   return [...my_string.replace(/[^\d]/g, '')]
+//     .sort((a, b) => a - b)
+//     .map(Number);
+// }
+
+// ---
+
+// function solution(my_string) {
+//   return [...my_string.replace(/[^\d]/g, '')].reduce(
+//     (acc, val) => acc + Number(val),
+//     0
+//   );
+// }
+
+// ---
+
+function solution(n) {
+  const answer = new Set();
+
+  for (let divisor = 2; n > 1; divisor++) {
+    if (n % divisor === 0) {
+      answer.add(divisor);
+      n /= divisor;
+      divisor = 1;
+    }
+  }
+
+  return [...answer];
 }
