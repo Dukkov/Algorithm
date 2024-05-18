04837804594508337dcc258f76f683e27e69b0ab
dukkov <dukov.ko.dev@gmail.com>
2024-05-18 23:33:04 +0900

    Day13 done

--- javascript/Introduction/0516.js
+++ javascript/Introduction/0516.js
@@ -1,4 +1,34 @@
-function solution(s) {
-  var answer = 0;
-  return answer;
+// function solution(s) {
+//   let previous = s.split(' ')[0];
+//   let answer = 0;
+
+//   for (val of s.split(' ')) {
+//     if (val === 'Z') answer -= previous;
+//     else {
+//       previous = Number(val);
+//       answer += Number(val);
+//     }
+//   }
+
+//   return answer;
+// }
+
+// ---
+
+// function solution(strlist) {
+//   return strlist.map((str) => str.length);
+// }
+
+// ---
+
+// function solution(my_string) {
+//   return [...new Set(my_string)].join('');
+// }
+
+// ---
+
+function solution(sides) {
+  const sortedArr = sides.sort((a, b) => a - b);
+
+  return sortedArr[0] + sortedArr[1] > sortedArr[2] ? 1 : 2;
 }
