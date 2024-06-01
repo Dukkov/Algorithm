cbbbdb67a0a7a1198e5f70a4c4697d03a9553f5d
Dukov <rhtkdals135@gmail.com>
2024-06-01 23:55:12 +0900

    Day19 processing

---
+++ javascript/Introduction/0601.js
@@ -0,0 +1,38 @@
+// function solution(array) {
+//   const arrayString = array.join('');
+
+//   return arrayString.split('').reduce((acc, cur) => {
+//     if (cur === '7') acc += 1;
+
+//     return acc;
+//   }, 0);
+// }
+
+// ---
+
+// function solution(my_str, n) {
+//   const answer = [];
+
+//   for (let i = 0; i < my_str.length; i += n) {
+//     answer.push(my_str.slice(i, i + n));
+//   }
+
+//   return answer;
+// }
+
+// ---
+
+// function solution(array, n) {
+//   return array.reduce((acc, cur) => {
+//     if (cur === n) acc++;
+
+//     return acc;
+//   }, 0);
+// }
+
+// ---
+
+function solution(array, height) {
+  var answer = 0;
+  return answer;
+}
