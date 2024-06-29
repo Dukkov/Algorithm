5fa5fc3bbc380cebe755ac508c9c9716d2cc3083
Dukov <rhtkdals135@gmail.com>
2024-06-29 23:56:37 +0900

    Day38 progressing

---
+++ javascript/level 1/12917.js
@@ -0,0 +1,8 @@
+// 문자열 내림차순으로 배치하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12917
+function solution(s) {
+  return s
+    .split('')
+    .sort((a, b) => b.localeCompare(a))
+    .join('');
+}
