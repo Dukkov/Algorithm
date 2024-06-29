3a68926ebee22a7609e1e44b2c7fcfced60de0d2
Dukov <rhtkdals135@gmail.com>
2024-06-30 00:11:07 +0900

    Day38 done

--- javascript/level 1/12917.js
+++ javascript/level 1/12917.js
@@ -1,8 +1,10 @@
 // 문자열 내림차순으로 배치하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/12917
 function solution(s) {
-  return s
-    .split('')
-    .sort((a, b) => b.localeCompare(a))
-    .join('');
+  return String.fromCharCode(
+    ...s
+      .split('')
+      .map((char) => char.charCodeAt(0))
+      .sort((a, b) => b - a)
+  );
 }
