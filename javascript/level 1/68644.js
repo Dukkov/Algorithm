b970c6a861da27f0b87bf273430cc96a1e439bf1
dukkov <dukov.ko.dev@gmail.com>
2024-07-07 23:52:35 +0900

    Day46 in progress

---
+++ javascript/level 1/68644.js
@@ -0,0 +1,13 @@
+// 두 개 뽑아서 더하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/68644
+function solution(numbers) {
+  const answer = new Set();
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.add(numbers[i] + numbers[j]);
+    }
+  }
+
+  return Array.from(answer).sort((a, b) => a - b);
+}
---
+++ javascript/level 1/81301.js
@@ -0,0 +1,6 @@
+// 숫자 문자열과 영단어
+// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+function solution(s) {
+  var answer = 0;
+  return answer;
+}
