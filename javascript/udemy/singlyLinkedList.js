b0215459fc5c6d62a006f837ebb1fa3f8beb832d
dukkov <dukov.ko.dev@gmail.com>
2024-10-10 07:05:30 +0900

    Section 19 done

--- javascript/udemy/singlyLinkedList.js
+++ javascript/udemy/singlyLinkedList.js
@@ -58,30 +58,99 @@ class SinglyLinkedList {
   }
 
   shift() {
-    this.head = this.next;
-    this.length--;
+    if (this.head) {
+      const prevHead = this.head;
+      this.head = this.head.next;
+      this.length--;
 
-    return this;
+      return prevHead;
+    } else {
+      return undefined;
+    }
   }
 
   unshift(val) {
     const newNode = new Node(val);
-    newNode.next = this.head;
-    this.head = newNode;
+
+    if (this.head) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
     this.length++;
 
     return this;
   }
 
-  get(num) {}
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return undefined;
 
-  set(num) {}
+    let element = this.head;
 
-  insert(num) {}
+    for (let counter = 0; counter < idx; counter++) {
+      element = element.next;
+    }
 
-  remove(num) {}
+    return element;
+  }
 
-  reverse() {}
+  set(val, idx) {
+    const getResult = this.get(idx);
+
+    if (getResult) {
+      getResult.val = val;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  insert(val, idx) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === 0) return !!this.unshift(val);
+    if (idx === this.length) return !!this.push(val);
+
+    const newNode = new Node(val);
+    const getResult = this.get(idx - 1);
+    newNode.next = getResult.next;
+    getResult.next = newNode;
+    this.length++;
+
+    return true;
+  }
+
+  remove(idx) {
+    if (idx < 0 || idx >= this.length) return undefined;
+    if (idx === 0) return this.shift();
+    if (idx === this.length - 1) return this.pop();
+
+    const getResult = this.get(idx - 1);
+    const removedNode = getResult.next;
+    getResult.next = removedNode.next;
+    this.length--;
+
+    return removedNode;
+  }
+
+  reverse() {
+    let current = this.head;
+    let prev = null;
+    let tmp;
+    this.head = this.tail;
+    this.tail = current;
+
+    for (let i = 0; i < this.length; i++) {
+      tmp = current.next;
+      current.next = prev;
+      prev = current;
+      current = tmp;
+    }
+
+    return this;
+  }
 }
 
 const sll = new SinglyLinkedList();
@@ -97,3 +166,22 @@ console.log(sll.pop());
 console.log(sll);
 console.log(sll.pop());
 console.log(sll);
+console.log(sll.push(11));
+console.log(sll.push(13));
+console.log(sll.shift());
+console.log(sll);
+console.log(sll.unshift(10));
+console.log(sll.pop());
+console.log(sll.pop());
+console.log(sll.unshift(5));
+console.log(sll.unshift(6));
+console.log(sll.get(1));
+console.log(sll.set(30, 2));
+console.log(sll);
+console.log(sll.insert(11, 0));
+console.log(sll);
+console.log(sll.remove(1));
+console.log(sll);
+console.log(sll.push(20));
+console.log(sll.push(30));
+console.log(sll.reverse());
